a2ca46dddb89bc9bc5a01aced62aaaa180e39bc2
heuristic_machina <machine_learner@yahoo.com>
2025-02-15 00:37:08 -0700

    removed nextjs plugin, added spa redirect rule + security headers, & nextjs output to standalone

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
+  env: {
+    // Add your env variables here
+    RAPIDAPI_KEY,
+    API_HOST
+  }
 }
 
 module.exports = nextConfig 
